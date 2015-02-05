71f418eb71e57fb94ad20ae17e4cf3365c03d2b2
wateer <liningning@wandoujia.com>
2015-02-05 16:32:29 +0800

    add date_diff.py

---
+++ date_diff.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+#-*- coding: utf-8 -*-
+
+""" 时间相比的例子, 要在 Python2.7 以上运行.
+
+"""
+
+import sys
+import json
+import requests
+import datetime
+
+
+def main():
+    _time  = "201502051500"
+    sub  = datetime.datetime.now() - datetime.datetime.strptime(_time, '%Y%m%d%H%M')
+    print sub.total_seconds()
+
+
+if __name__ == "__main__":
+    main()
