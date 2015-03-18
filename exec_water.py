a9947df723ded8b4feffc935807f28b925ddb2b6
wateer <liningning@wandoujia.com>
2015-03-18 18:09:58 +0800

    add exec_water.py

---
+++ exec_water.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import subprocess
+
+
+def exec_water(cmd):
+    """ 执行命令, 循环输出执行结果 """
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    while proc.poll() is None:
+        line = proc.stdout.readline()
+        if not line: continue
+        print line[:-1]
+
+
+if __name__ == "__main__":
+    cmd = "echo 1; sleep 1 ;echo 2"
+    exec_water(cmd)
