002d89fb1f7707f7e43566eb42881185fef0ee08
wateer <liningning@wandoujia.com>
2015-06-08 14:08:39 +0800

    add date_util.py

--- log.py
+++ log.py
@@ -9,10 +9,6 @@ from config import log_file
 
 
 def get_logger():
-    '''
-    @summary: init logger
-    @result: return a logger object
-    '''
     logger_ = logging.getLogger("employee_account")
     formatter = logging.Formatter
     formatter = logging.Formatter(
@@ -23,7 +19,9 @@ def get_logger():
     logger_.setLevel(logging.DEBUG)
     return logger_
 
+
 if not os.path.isdir(log_dir):
     os.makedirs(log_dir)
+
 
 logger = get_logger()
