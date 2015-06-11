452a8acc52b6e7d164a5d93c914d578cc19328d0
wateer <liningning@wandoujia.com>
2015-06-11 14:54:08 +0800

    add urllib2_http_request.py

---
+++ http_utils/urllib2_http_request.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" 一个不需要验证密码的 http 请求工具.
+
+"""
+
+import urllib
+import urllib2
+
+import ujson as json
+
+
+HOST = ""
+
+
+class HTTPRequstClass(object):
+    def __init__(self, host_url=HOST):
+        self.is_login = False
+        self.host_url = host_url
+
+    def post_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url, data)
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
+
+    def get_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url + "?" + data)
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
+
+    def put_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url, data)
+        request.get_method = lambda: 'PUT'
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
+
+    def delete_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url + "?" + data)
+        request.get_method = lambda: 'DELETE'
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
+
+    def patch_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url + "?" + data)
+        request.get_method = lambda: 'PATCH'
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
--- http_utils/urllib2_redirect_cookie.py
+++ http_utils/urllib2_redirect_cookie.py
@@ -50,9 +50,9 @@ class LoginException(Exception):
         return str(self.__data)
 
 
-class Ldapapi(object):
-    def __init__(self, host_url=HOST, username=AUTH_USERNAME, \
-            password=AUTH_PASSWD, auth_uri=AUTH_API):
+class HTTPRequestClass(object):
+    def __init__(self, host_url=HOST, username=AUTH_USERNAME, 
+                 password=AUTH_PASSWD, auth_uri=AUTH_API):
         self.is_login = False
         self.host_url = host_url
         self.username = username
@@ -102,7 +102,7 @@ class Ldapapi(object):
 
         return ret_dict
 
-    def put_wrapper(cls, url, data_dict):
+    def put_wrapper(self, url, data_dict):
         data = urllib.urlencode(data_dict)
         visit_url = r"http://" + self.host_url + r"/" + url
 
@@ -115,12 +115,25 @@ class Ldapapi(object):
 
         return ret_dict
 
-    def delete_wrapper(cls, url, data_dict):
+    def delete_wrapper(self, url, data_dict):
         data = urllib.urlencode(data_dict)
         visit_url = r"http://" + self.host_url + r"/" + url
 
         request = urllib2.Request(visit_url + "?" + data)
         request.get_method = lambda: 'DELETE'
+        request.add_header("Cookie", self.cookies)
+        login_response = urllib2.urlopen(request)
+        response = login_response.read()
+        ret_dict = json.loads(response)
+
+        return ret_dict
+
+    def patch_wrapper(self, url, data_dict):
+        data = urllib.urlencode(data_dict)
+        visit_url = r"http://" + self.host_url + r"/" + url
+
+        request = urllib2.Request(visit_url + "?" + data)
+        request.get_method = lambda: 'PATCH'
         request.add_header("Cookie", self.cookies)
         login_response = urllib2.urlopen(request)
         response = login_response.read()
